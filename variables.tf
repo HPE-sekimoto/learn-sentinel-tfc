0b4c725819e0eb261e80edc48968ca11660d7e4b
ken.sekimoto <HPE-sekimoto@users.noreply.github.com>
2024-09-02 09:03:37 +0900

    Update files

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "ap-northeast-1"
 }
 
 variable "instance_type" {
@@ -11,4 +11,16 @@ variable "instance_type" {
 variable "instance_name" {
   description = "EC2 instance name"
   default = "Provisioned by Terraform"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+  type        = string
+  sensitive   = true
 }
--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = ">= 3.0.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.26.0"
+      version = "~> 5.0"
     }
   }
 
